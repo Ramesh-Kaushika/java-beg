fec3f2b1150500d9f28222cd3c83d4db2c803b8f
Ramesh Kaushika De Silva <149869212+Ramesh-Kaushika@users.noreply.github.com>
2024-01-20 10:35:14 +0530

    2024.01.20

---
+++ IDE/Demo-afsd/src/data/Node.java
@@ -0,0 +1,15 @@
+package data;
+
+public class Node {
+
+    int data;
+
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+
+    }
+
+
+}
---
+++ IDE/Demo-afsd/src/data/Stack.java
@@ -0,0 +1,33 @@
+package data;
+
+public class Stack {
+
+    private Node top;
+
+    public void push(int data){
+
+        Node node = new Node(data);
+        node.next=top;
+        top=node;
+
+    }
+
+    public void printStack(){
+//same reference variable dont data iterate because top ref variable damage
+        /*while(top!=null){
+
+            System.out.println(top.data);
+            top=top.next;
+        }*/
+
+        // top sign another variable
+        Node temp = top;
+
+        while (temp!=null){
+            System.out.println(top.data);
+            temp=temp.next;
+
+        }
+    }
+
+}
